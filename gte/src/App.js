1bf7ab8ce4a4defb37308dba0cd9441801b382fa
Blake Izaguirre <blakeizaguirre@gmail.com>
2024-01-12 12:34:21 -0500

    stubbed into app.js

--- gte/src/App.js
+++ gte/src/App.js
@@ -1,22 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import DiagramDisplay from './DiagramDisplay';
+import BusinessProcessForm from './BusinessProcessForm';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <DiagramDisplay />
+        <BusinessProcessForm />
       </header>
     </div>
   );
